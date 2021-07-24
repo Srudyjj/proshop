c7524fe5be76fd1dcc901ebdc955cfd38b17c16c
Serhii Rudyi <srudyjj@gmail.com>
2021-07-24 13:58:15 +0300

    4.21 Create model

---
+++ backend/models/Product.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const revievsSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      require: true,
+    },
+    image: {
+      type: String,
+      require: true,
+    },
+    brand: {
+      type: String,
+      require: true,
+    },
+    catedory: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    revievs: [revievsSchema],
+    rating: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      require: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
